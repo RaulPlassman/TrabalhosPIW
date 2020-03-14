1defcb21b6fe8086d8f81c58e12e567c4b1a53fc
Raul Plassman <39928730+RaulPlassman@users.noreply.github.com>
2020-03-13 22:08:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavSuperior from './componentes/NavSuperior'
+import Galeria from './componentes/Galeria';
+
+function App() {
+  return (
+    <div>
+    <NavSuperior/>
+    <Galeria></Galeria>
+    </div>
+  );
+}
+
+export default App;
